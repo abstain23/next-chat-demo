01e931464c812007d29d80cffaf973821937c5f0
cc <cc@qq.com>
2023-03-25 22:48:43 +0800

    feat: init next pro

---
+++ pages/api/chat.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { ChatGPTAPI } from 'chatgpt'
+
+console.log('process.env.CHAT_API_KEY',process.env.CHAT_API_KEY)
+const api = new ChatGPTAPI({
+  apiKey: process.env.CHAT_API_KEY!,
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  try {
+    const search = req.query.s
+    if (!search) {
+      res.status(400).json({
+        msg: 'param error'
+      })
+      return  
+    }
+    const message = await api.sendMessage("如何学习k8s?")
+    return res.json(message.text)
+  } catch (error) {
+    console.log('error =>', error)
+    return res.status(500).json({
+      msg: 'time out'
+    })
+  }
+}
